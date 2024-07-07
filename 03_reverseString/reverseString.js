5ba2fbb2d41d5f8145e25dd02182ca9a106bf453
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-07 12:42:13 +0530

    Add comments to explain the code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,11 @@
 const reverseString = function(string) {
-    let newString = String(string);
-    let lenOfString = newString.length;
-    let revStr = "";
+    let newString = String(string); //Convert to string
+    let lenOfString = newString.length; //get the length of the string and store it
+    let revStr = "";    //To hold the reversed string
+
+    // The loop runs gets all the characters of the string
+    // starting from the last to the first,
+    // and adds them to the reversed string.
     for (let i=lenOfString-1; i>=0; i--) {
         let char = newString.charAt(i);
         revStr += char;
