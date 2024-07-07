59d073a523c69f21495de00532f138907f077e15
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-07 12:19:50 +0530

    Pass all the 7 tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    string = String(string);
+    num = Number(num);
 
+    let newString="";
+    if (num<0)  {
+        return "ERROR";
+    } else {
+        for (let i=1; i<=num; i++) {
+            newString += string;
+        }
+        return newString;    
+    }
 };
 
 // Do not edit below this line
