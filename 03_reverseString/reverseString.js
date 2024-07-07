bdadea2035d01d88d4d7d238b13f07d27207e433
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-07 12:38:50 +0530

    Pass all the 4 tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = String(string);
+    let lenOfString = newString.length;
+    let revStr = "";
+    for (let i=lenOfString-1; i>=0; i--) {
+        let char = newString.charAt(i);
+        revStr += char;
+    }
+    return revStr;
 };
 
 // Do not edit below this line
