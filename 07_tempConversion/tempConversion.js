e1ccfa302d00573ca1e699c63244e1c9a322a159
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-09 13:49:14 +0530

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempCelsius = (tempInFahrenheit-32)*(5/9);
+  return Math.round(tempCelsius*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempFahrenheit = (tempInCelsius*(9/5))+32;
+  return Math.round(tempFahrenheit*10)/10;
 };
 
 // Do not edit below this line
