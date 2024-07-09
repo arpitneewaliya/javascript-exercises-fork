805672c3cb32d3f73245870800b88c8afe2834e5
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-09 12:42:56 +0530

    All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1<0 || num2<0) {
+        return "ERROR";
+    } else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else {
+        let sum = 0;
+        if (num1 < num2) {
+            let i = num1;
+            while (i <= num2) {
+                sum += i;
+                i++;
+            }
+            return sum;
+        } else if (num1 > num2) {
+            let i = num1;
+            while (i >= num2) {
+                sum += i;
+                i--;
+            }
+            return sum;   
+        }
+    }
 };
 
 // Do not edit below this line
