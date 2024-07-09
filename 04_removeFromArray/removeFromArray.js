ead89a7b76154a4e2e7add2d1d99dcd4e8af4444
arpitneewaliya <arpit.neewaliya@gmail.com>
2024-07-09 12:43:41 +0530

    All tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...otherArgs) {
+    let newArray = [...myArray];   // Makes a copy of myArray, now myArray will not change even if newArray changes
+
+    let finalArr;
+    for (let element of otherArgs) {
+        finalArr = newArray.filter((value) => value !== element);
+        newArray = [...finalArr];
+    }
+
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
